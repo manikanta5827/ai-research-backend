f74235c41ba7aab50eec4995626b180b06afab21
Manikanta Thummuri <postbox5827@gmail.com>
2025-09-20 22:57:45 +0530

    update: auth handler

--- src/middleware/authHandler.js
+++ src/middleware/authHandler.js
@@ -1,40 +1,14 @@
-const { verifyAuthToken } = require("../services/authService.js");
-const {PrismaClient} = require('@prisma/client');
-const prisma = new PrismaClient();
 
-const authHandler = async(req,res,next) => {
-    const authToken = req.headers['auth-token'];
+const authHandler = async(req, res, next) =>{
+    const userId = req.headers['userId'];
 
-    if(!authToken) {
+    if(!userId) {
         return res.status(400).json({
             status: "error",
-            message: "Auth token required"
+            message: "user id required"
         })
     }
-
-    // validate the auth token
-    const response = verifyAuthToken(authToken);
-    if(!response.status) {
-        return res.status(401).json({
-            status: "error",
-            message: response.data || "Invalid Token"
-        })
-    }
-
-    // keep the user data in req body
-    const user = await prisma.user.findUnique({
-        where: {
-            id
-        }
-    })
-
-    if(!user) {
-        return res.status(404).json({
-            status: "error",
-            message: "user not found"
-        })
-    }
-    req.user = user;
+    req.userId = userId;
     next();
 }
 
