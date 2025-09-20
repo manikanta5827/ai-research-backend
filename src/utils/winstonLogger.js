2fd30cf52717b5e2704efc025bedc170666ad7c1
Manikanta Thummuri <postbox5827@gmail.com>
2025-09-21 00:40:35 +0530

    update: add api finish calculation event

--- src/utils/winstonLogger.js
+++ src/utils/winstonLogger.js
@@ -32,4 +32,22 @@ const logger = createLogger({
   ],
 });
 
-module.exports = logger;
+const logMiddleware = (req, res, next) => {
+  const startHrTime = process.hrtime();
+
+  res.on('finish', () => {
+      const elapsedHrTime = process.hrtime(startHrTime);
+      const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6;
+      const logMessage = `${req.method} ${req.originalUrl} ${res.statusCode} ${elapsedTimeInMs.toFixed(2)}ms`;
+
+      if (res.statusCode >= 400) {
+          logger.error(logMessage);
+      } else {
+          logger.info(logMessage);
+      }
+  });
+
+  next();
+};
+
+module.exports = {logger, logMiddleware};
