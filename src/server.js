5d0da39c08f911a8b6ccd97ae2174ebb13bdbe55
Manikanta Thummuri <postbox5827@gmail.com>
2025-09-20 22:01:14 +0530

    create basic project

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const reqStack = require('request-stack');
+const errorHandler = require('./middleware/errorHandler.js');
+const appRoutes = require('./routes/appRoutes.js');
+
+// middleware
+app.use(morgan('dev'));
+app.use(reqStack);
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: "success",
+        message: "Server is running",
+        timestamp: new Date().toISOString(),
+    });
+});
+
+//router
+app.use('/', appRoutes);
+
+//error handler
+app.use(errorHandler);
+
+
+const PORT = process.env.PORT || 3400;
+app.listen(PORT,()=>{
+    console.log('HTTP server listening on port...', PORT);
+})
