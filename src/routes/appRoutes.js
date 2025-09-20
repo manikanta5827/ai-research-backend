f2711b74fc3a8143522639cfa8321ded2292fe7f
Manikanta Thummuri <postbox5827@gmail.com>
2025-09-20 22:58:37 +0530

    update: implement api's in routes

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const authHandler = require('../middleware/authHandler.js');
+const appController = require('../controllers/appController.js');
 
+
+// all routes
+router.post('/research', authHandler, appController.triggerResearch);
+// router.get('/research', appController)
 
 
 
