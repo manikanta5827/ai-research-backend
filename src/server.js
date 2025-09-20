dab16cff2f462860f3ef596745e226498ec65c39
Manikanta Thummuri <postbox5827@gmail.com>
2025-09-21 00:42:52 +0530

    update: improve middleware functions

--- src/server.js
+++ src/server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const reqStack = require('request-stack');
+const { logMiddleware } = require('./utils/winstonLogger.js');
 const errorHandler = require('./middleware/errorHandler.js');
 const appRoutes = require('./routes/appRoutes.js');
 require('dotenv').config();
 
 // middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(reqStack);
+app.use(logMiddleware);
 
 app.get('/', (req, res) => {
     res.status(200).json({
@@ -26,6 +30,6 @@ app.use(errorHandler);
 
 
 const PORT = process.env.PORT || 3400;
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log('HTTP server listening on port...', PORT);
 })
