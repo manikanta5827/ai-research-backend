072518992a0eedde6d4bfb8e5c0a4bea064dcf2f
Manikanta Thummuri <postbox5827@gmail.com>
2025-09-21 18:12:52 +0530

    update: completed the flow of ai agent handling

--- src/messageHandlers/aiAgentMessageHandler.js
+++ src/messageHandlers/aiAgentMessageHandler.js
@@ -1,40 +1,73 @@
-const { logger } = require("../utils/winstonLogger");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+require('dotenv').config();
+
+const { updateLogs } = require('../repository/logsRepository.js');
+const { updateProgress } = require('../repository/taskRepository.js');
+const { updateFinalTaskResult } = require("../repository/taskResultRepository.js")
+const { taskStatus } = require('../enums/taskStatusEnum.js');
+const { validateTopic, searchUsingDuckDuck, generateSummaryUsingLLM, generateSingleSummary } = require('../services/aiAgentMessageHandlerService.js');
+
 
 const aiAgentMessageHandler = async (job) => {
-    const topic = job.data.topic;
-    // validate the topic if it is less than 3 words then don't search
+    try {
+        const { id, topic } = job.data;
 
+        // validate the topic
+        await validateTopic(id, topic);
 
-    return { result: "Done", taskId: job.data.id };
+        // fetch content from websites using duck duck fast api server
+        const data = await searchUsingDuckDuck(id, topic);
 
-}
+        let final_response = {
+            topic: topic,
+            articles: [],
+            final_synthesis: {
+                overview: "",
+                keywords: []
+            }
+        };
 
-async function updateLogs(taskId, step, message, meta = {}) {
-    if (!taskId) {
-        logger.error("taskId not found")
-        throw new Error("taskId not found");
+        let keywords = [];
+        let summary = [];
+        // hit llm for every web page to get summary
+        await Promise.all(data.map(async (webpage, index) => {
+            const response = await generateSummaryUsingLLM(webpage.title, webpage.content);
+
+            final_response.articles.push({
+                url: webpage.url,
+                title: webpage.title,
+                summary: response.summary,
+                keywords: response.keywords
+            })
+
+            summary.push(response.summary);
+            keywords.push(response.keywords);
+
+            // update progress
+            await updateLogs(id, 'ai_summarization', `sent index ${index + 1} webpage content to Gemini API for summarisation`, { "api": "Gemini", "response_length": response.summary.length });
+        }))
+        console.log('succesfully hit all llm api calls');
+        await updateProgress(id, 75, taskStatus.RUNNING);
+
+        // console.log(`summary::${summary}`);
+        // console.log(`keywords::${keywords}`);
+        // hit single llm api to get final summary
+        const response = await generateSingleSummary(summary, keywords);
+        final_response.final_synthesis.overview = response.summary;
+        final_response.final_synthesis.keywords = response.keywords;
+
+        await updateLogs(id, 'completion', `task succeeded`, { "num_articles": data.length, "response_length": response.length });
+        await updateProgress(id, 100, taskStatus.RUNNING, true);
+        await updateFinalTaskResult(id, final_response);
+
+        console.log(`topic fetching completed for the job of id::${job.id}`);
+        return { result: "topic fetching completed", taskId: id };
+    } catch (error) {
+        logger.info(`Error happened in job id:: ${job.id} due to ${error.message}`);
+        await updateProgress(job.data.id, 0, taskStatus.FAILED, true, error.message);
+        await updateFinalTaskResult(id, { error: "error happened during handling" })
     }
-
-    if (!step) {
-        logger.error("step not found")
-        throw new Error("step not found");
-    }
-
-    if (!message) {
-        logger.error("message not found")
-        throw new Error("message not passed")
-    }
-
-    await prisma.log.create({
-        data: {
-            taskId,
-            step,
-            message,
-            meta
-        }
-    })
 }
 
 module.exports = aiAgentMessageHandler;
