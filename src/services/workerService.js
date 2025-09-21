0fa77f64ff411b92e8a66131a86c9ae2f6c7f8a6
Manikanta Thummuri <postbox5827@gmail.com>
2025-09-21 18:11:13 +0530

    update: add spacing and integrate update progress method for failed jobs

--- src/services/workerService.js
+++ src/services/workerService.js
@@ -1,7 +1,9 @@
 const { Worker } = require('bullmq');
 const connection = { host: '127.0.0.1', port: 6379 };
-const {logger} = require('../utils/winstonLogger.js');
+const { logger } = require('../utils/winstonLogger.js');
 const aiAgentMessageHandler = require('../messageHandlers/aiAgentMessageHandler.js');
+const { taskStatus } = require('../enums/taskStatusEnum.js');
+const { updateProgress } = require('../repository/taskRepository.js');
 
 const worker = new Worker(
   'myQueue',
@@ -9,10 +11,10 @@ const worker = new Worker(
     logger.info(`Processing job ${job.name}`);
 
     switch (job.name) {
-        case 'ai-agent-queue':
-            return await aiAgentMessageHandler(job);
-        default:
-          throw new Error(`Unknown job type: ${job.name}`);
+      case 'ai-agent-queue':
+        return await aiAgentMessageHandler(job);
+      default:
+        throw new Error(`Unknown job type: ${job.name}`);
     }
   },
   { connection }
@@ -26,8 +28,9 @@ worker.on('error', (err) => {
   logger.error('Worker error:', err);
 });
 
-worker.on('stalled', (jobId) => {
+worker.on('stalled', async (jobId) => {
   logger.warn(`Job ${jobId} has stalled`);
+  await updateProgress(jobId, 0, taskStatus.FAILED, true, "job stalled more than allowable limit");
 });
 
 worker.on('progress', (job, progress) => {
@@ -38,8 +41,9 @@ worker.on('completed', job => {
   logger.info(`Job ${job.id} has been completed`);
 });
 
-worker.on('failed', (job, err) => {
+worker.on('failed', async (job, err) => {
   logger.error(`Job ${job.id} failed:`, err);
+  await updateProgress(job.id, 0, taskStatus.FAILED, true, err);
 });
 
 worker.on('connectionError', (err) => {
