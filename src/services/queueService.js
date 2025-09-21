8838f4568c4ae8334636ef816a0b5f067d7f0d98
Manikanta Thummuri <postbox5827@gmail.com>
2025-09-21 20:49:02 +0530

    update: add redis host to env

--- src/services/queueService.js
+++ src/services/queueService.js
@@ -1,5 +1,13 @@
 const { Queue } = require('bullmq');
-const connection = { host: '127.0.0.1', port: 6379 };
+require('cors').config();
+
+const REDIS_HOST_URL= process.env.REDIS_HOST_URL;
+
+if(!REDIS_HOST_URL) {
+    throw new Error("redis host is not specified in env file")
+}
+
+const connection = { host: REDIS_HOST_URL, port: 6379 };
 
 const myQueue = new Queue('myQueue', { connection });
 
